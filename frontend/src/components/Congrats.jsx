5d5800db223bba3e997c7c6f492b7d8523ccf4eb
mabel <bigbelk@live.ru>
2021-01-15 17:51:24 +0300

    no_eslint

--- frontend/src/components/Congrats.jsx
+++ frontend/src/components/Congrats.jsx
@@ -2,7 +2,7 @@ export default function(props){
     return (
       <div className="showcase">
         <h1>Congratulations!</h1>
-        <p>Your order will be delivered as soon as possible (especially considering that it was not really sent to server 🤣). </p>
+        <p>Your order will be delivered as soon as possible{/* (especially considering that it was not really sent to server 🤣)*/}. </p>
       </div>
     )
 }
